c693dc2d8c5e3726ea1353bcc2008493bb7fd767
M mx <1624045573tx@gmail.com>
2020-11-13 14:31:45 +0800

    完善url上报

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	"net/url"
 	"os"
 	"strconv"
 	"time"
@@ -25,18 +26,16 @@ func checkFileIsExist(filename string) bool {
 	return true
 }
 
-func loger() {
-	// 级别为info warning none
-}
-
 func worker() {
 	//计数者
 	b := sign
 	for {
 		time.Sleep(time.Duration(1) * time.Second)
-		popes[sign].a--
-		if popes[sign].a < 0 {
-			tprint(popes[sign].b + " 超时")
+		popes[b].a--
+		if popes[b].a < 0 {
+			msg := "「" + popes[b].b + "」(" + popes[b].c + ") 宕机"
+			tprint(msg)
+			urler(msg, popes[b].b, 0, "down")
 			delete(popes, b)
 			dpper[b] = &dpp{time.Now().Unix()}
 			break
@@ -68,6 +67,11 @@ func timecount(a string) string {
 	return a
 }
 
+func urler(msg string, name string, time int64, mtype string) {
+	_, err := http.Get(surl + "?msg=" + url.QueryEscape(msg) + "&name=" + url.QueryEscape(name) + "&time=" + strconv.FormatInt(time, 10) + "&type=" + mtype)
+	errer("上报URL请求失败", err)
+}
+
 type pp struct {
 	a int
 	b string
@@ -83,21 +87,19 @@ var dpper map[string]*dpp = make(map[string]*dpp)
 //接收get参数
 var name string
 var sign string
+var surl string
 
 func main() {
 	//接收参数
 	var port string
 	var path string
-	var log string
-	var url string
 	flag.StringVar(&port, "p", "233", "监听端口")
 	flag.StringVar(&path, "path", "/", "心跳路径")
-	flag.StringVar(&log, "log", "info", "日志基本")
-	flag.StringVar(&url, "url", "", "上报URL")
+	flag.StringVar(&surl, "url", "", "上报URL")
 	flag.Parse()
 
 	//检查参数
-	if url == "" {
+	if surl == "" {
 		fmt.Println("上报URL为必填项")
 		os.Exit(3)
 	}
@@ -120,24 +122,28 @@ func main() {
 				w.Write([]byte(`{"status":"fail","message":"sign参数有误"}`))
 				return
 			}
-			time, err := strconv.Atoi(get["time"][0])
+			ttime, err := strconv.Atoi(get["time"][0])
 			if err != nil {
 				errer("请求中的time参数有误", err)
 				w.Write([]byte(`{"status":"fail","message":"time参数有误"}`))
 				return
 			}
 			if popes[sign] == nil {
-				popes[sign] = &pp{time, name, r.RemoteAddr}
+				popes[sign] = &pp{ttime, name, r.RemoteAddr}
 				go worker()
 				if dpper[sign] != nil {
-					tprint("「" + name + "」恢复，历时" + timecount(sign))
+					msg := "「" + name + "」(" + r.RemoteAddr + ") 恢复，历时" + timecount(sign)
+					tprint(msg)
+					urler(msg, name, time.Now().Unix()-dpper[sign].a, "up")
 					delete(dpper, sign)
 				} else {
-					tprint("新主机「" + name + "」上线")
+					msg := "新主机「" + name + "」(" + r.RemoteAddr + ") 上线"
+					tprint(msg)
+					urler(msg, name, 0, "new")
 				}
 				w.Write([]byte(`{"status":"ok","data":"init"}`))
 			} else {
-				popes[sign].a = time
+				popes[sign].a = ttime
 				popes[sign].b = name
 				popes[sign].c = r.RemoteAddr
 				w.Write([]byte(`{"status":"ok","data":"continue"}`))
